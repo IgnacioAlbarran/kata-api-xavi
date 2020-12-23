6e32b2bbeee608f14c77384b4bcd6d8440c4a6c1
Ignacio Albarrán <ignacioalbarran851@gmail.com>
2020-12-23 11:24:09 +0100

    base de datos configurada y conectable: puerto 3007 local a puerto 3006 de docker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
-
+import { createConnection, Repository, getCustomRepository } from "typeorm"
 
 const app = express();
 app.use(express.json);
 
+(async ()=> {
+    try{
+        const connection = await createConnection();
+        console.log("Connection stablished? : ", connection.isConnected);
+    }catch(error){
+        console.error(error)
+    }
+})()
 
 app.listen(3000, () => {
     console.log('Ready on port 3000!');
