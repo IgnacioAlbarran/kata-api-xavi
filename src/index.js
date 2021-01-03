5a4c1332ed92c8f35c44fe8b482295f46aa5ae13
Ignacio Albarrán <ignacioalbarran851@gmail.com>
2021-01-03 22:03:10 +0100

    create all the routes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,73 @@
 import express from 'express';
-import { createConnection, Repository, getCustomRepository } from "typeorm"
+import { createConnection, Repository, getCustomRepository, getRepository } from "typeorm"
+import { Book } from './entities/book';
 import { BookRepository } from './repositories/bookRepository';
 
+const bodyParser = require('body-parser');
 const app = express();
-app.use(express.json);
+app.use(bodyParser.json());
 
 (async ()=> {
     try{
         const connection = await createConnection();
         console.log("Connection stablished? : ", connection.isConnected);
-        const createdBook = await new getCustomRepository(BookRepository).createBook("La historia interminable", "B23453", "Michael Ende");
-        console.log(createdBook);
-
     }catch(error){
         console.error(error)
     }
 })()
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+})
+
+// book index route
+app.get('/books', async (req, res) => {
+    const bookList = await new getCustomRepository(BookRepository).getList();
+    res.json(bookList);
+})
+
+// book route
+app.get("/book/:identifier", async(req, res) => {
+    var { identifier } = req.params;
+    var book = await new getCustomRepository(BookRepository).getBook(identifier)
+    console.log(book)
+    res.send(book)
+})
+
+// // book route
+// app.get("/book/:isbn", async(req, res) => {
+//     var { isbn } = req.params;
+//     console.log(req.params)
+//     var book = await new getCustomRepository(BookRepository).getBook(isbn)
+//     res.send(book)
+// })
+
+// create book route
+app.post("/books", async(req, res) => {
+    const { title, isbn, author } = req.body;
+
+    const createdBook = await new getCustomRepository(BookRepository).createBook(title, isbn, author)
+        .then(createdBook => res.send(createdBook))
+        .catch(error => console.error(error))
+        //// ESTO ESTÁ BIEN HECHO??? NO SÉ SI ME ESTÁ CAPTURANDO LOS ERRORES, PORQUE ME SALIÓ UNO POR CONSOLA, PERO NUNCA MAS, Y POR POSTMAN NO SALEN...
+})
+
+// update book route
+app.put('/books/:id', async(req, res) => {
+    const { id } = req.params
+    const newObject = req.body
+    const newBook = await new getCustomRepository(BookRepository).updateBook(id, newObject)
+        .then(newBook => res.send(newBook))
+        .catch(error => console.error(error));
+})
+
+// delete book route
+app.delete('/books/:id', async(req, res) => {
+    const { id } = req.params
+    const deletedBook = await new getCustomRepository(BookRepository).deleteBook(id)
+        .then(res.send("Eliminada la id: " + id))
+        .catch(error => console.error(error))
+})
 
 app.listen(3000, () => {
     console.log('Ready on port 3000!');
--- src/repositories/bookRepository.js
+++ src/repositories/bookRepository.js
@@ -4,11 +4,55 @@ import { Book } from "../entities/book.js"
 @EntityRepository(Book)
 export class BookRepository extends Repository{
   async createBook(title, isbn, author){
-    const book = new Book();
-    book.title = title;
-    book.isbn = isbn;
-    book.author = author;
+    try{
+      const book = new Book();
+      book.title = title;
+      book.isbn = isbn;
+      book.author = author;
+      return await this.save(book)
+    }
+    catch(error){
+      return error;
+    }
+  }
 
-  return await this.save(book)
+  async getList(){
+    try{
+      return await this.find()
+    }catch(error){
+      return error
+    }
+  }
+
+  async getBook(param){
+    try{
+      if (isNaN(Number.parseInt(param))){
+        return await this.findOne({isbn: param})
+      }else{
+        return await this.findOne({id: Number.parseInt(param)})
+      }
+    }catch(error){
+      return error
+    }
+  }
+
+  async deleteBook(id){
+    try{
+      const bookRemoved = this.findOne({id: id})
+      return await this.delete(id)
+    }catch(error){
+      return error
+    }
+  }
+
+  async updateBook(id, book){
+    try{
+      const deletedObject = await this.delete(id);
+      const newBook = book;
+      newBook.id = id;
+      return await this.save(newBook)
+    }catch(error){
+        return error;;
+    }
   }
 }
